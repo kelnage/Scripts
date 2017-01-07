f81553b874c8e1944b67ad3595dd7df025926b6f
Rafael <faelgs15@gmail.com>
2017-01-07 17:29:15 -0200

    Update BetterCommentChains.v1.0.js

--- rhSGST/BetterCommentChains.v1.0.js
+++ rhSGST/BetterCommentChains.v1.0.js
@@ -6,7 +6,7 @@ function BetterCommentChains(Context) {
 }
 
 function addBCCLink(Chain, Parent) {
-    var I, Username, ID, Chained;
+    var I, Username, ID, Chained, BCCLink;
     I = Chain.length;
     if (I > 0) {
         if (SG) Username = Parent.firstElementChild.getAttribute("href") ? Parent.firstElementChild.getAttribute("href").split("/user/")[1] : "Deleted";
@@ -18,10 +18,9 @@ function addBCCLink(Chain, Parent) {
         while (I > 0) {
             if (SG) Chained = Chain[--I].firstElementChild.firstElementChild.nextElementSibling.firstElementChild;
             else Chained = Chain[--I].firstElementChild.className == "comment_inner" ? Chain[I].firstElementChild.firstElementChild : Chain[I].firstElementChild.nextElementSibling.firstElementChild;
-            Chained.insertAdjacentHTML(
-                "beforeend",
-                "<a href=\"#" + ID + "\" style=\"color: rgb(107, 122, 140); margin-left: 5px;\">@" + Username + "</a>"
-            );
+            BCCLink = Chained.getElementsByClassName("BCCLink")[0];
+            if (BCCLink) BCCLink.textContent = Username;
+            else Chained.insertAdjacentHTML("beforeend", "<a class=\"BCCLink\" href=\"#" + ID + "\" style=\"color: rgb(107, 122, 140); margin-left: 5px;\">@" + Username + "</a>");
         }
     }
 }
