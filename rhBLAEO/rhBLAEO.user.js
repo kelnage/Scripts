80b8a40288b57e58387cfc3ae57e679dfd842e17
Rafael <faelgs15@gmail.com>
2017-01-08 12:58:37 -0200

    Update rhBLAEO.user.js

--- rhBLAEO/rhBLAEO.user.js
+++ rhBLAEO/rhBLAEO.user.js
@@ -3,51 +3,34 @@
 // @namespace revilheart
 // @author revilheart
 // @description Adds some cool features to BLAEO.
-// @version 1.0
+// @version 1.1
 // @match http://backlog-deepness.rhcloud.com/*
 // @match https://backlog-deepness.rhcloud.com/*
 // @grant GM_xmlhttpRequest
 // @grant GM_setValue
 // @grant GM_getValue
 // @grant GM_deleteValue
-// @require https://github.com/revilheart/Scripts/raw/master/rhBLAEO/SettingsMenu.v1.0.js
-// @require https://github.com/revilheart/Scripts/raw/master/rhBLAEO/ThemeListChecker.v1.0.js
+// @connect api.steampowered.com
+// @require https://github.com/revilheart/Scripts/raw/master/rhBLAEO/SettingsMenu.v1.1.js
+// @require https://github.com/revilheart/Scripts/raw/master/rhBLAEO/ThemeListChecker.v1.0.1.js
 // @run-at document-idle
 // ==/UserScript==
 
 var rhBLAEO;
-rhBLAEO = {
-    Features: {
-        TLC: {
-            Name: "Theme List Checker",
-            Enabled: true,
-        },
-    },
-    Storage: {
-        TLCList: null,
-        TLCGames: [],
-        UserID: null,
-        SteamID: null,
-        APIKey: null,
-        OwnedGames: null,
-    },
-};
-if (GM_getValue("rhBLAEO")) {
-    rhBLAEO = GM_getValue("rhBLAEO");
-} else {
-    GM_setValue("rhBLAEO", rhBLAEO);
-}
-if (window.location.href.match("/settings/")) {
-    SettingsMenu();
-}
+rhBLAEO = { Features: { TLC: { Name: "Theme List Checker", Enabled: true } }, Storage: { SteamAPIKey: "", SteamID: "", Username: "?", OwnedGames: [], LastSync: "", TLCList: "", TLCGames: [] } };
+if (GM_getValue("rhBLAEO")) rhBLAEO = GM_getValue("rhBLAEO");
+else GM_setValue("rhBLAEO", rhBLAEO);
 if (rhBLAEO.Storage.APIKey) {
-    ThemeListChecker();
+    rhBLAEO.Storage.SteamAPIKey = rhBLAEO.Storage.APIKey;
+    delete rhBLAEO.Storage.APIKey;
 }
+if (rhBLAEO.Storage.UserID) {
+    rhBLAEO.Storage.Username = rhBLAEO.Storage.UserID;
+    delete rhBLAEO.Storage.UserID;
+}
+if (window.location.href.match("/settings/")) SettingsMenu();
+if (rhBLAEO.Storage.SteamAPIKey) ThemeListChecker();
 document.addEventListener("turbolinks:load", function() {
-    if (window.location.href.match("/settings/")) {
-        SettingsMenu();
-    }
-    if (rhBLAEO.Storage.APIKey) {
-        ThemeListChecker();
-    }
+    if (window.location.href.match("/settings/")) SettingsMenu();
+    if (rhBLAEO.Storage.SteamAPIKey) ThemeListChecker();
 });
